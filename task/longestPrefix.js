a3b3fb598fb490a03fd1ef0398f6a9ebdf5d5cd8
modestran <modest.ran@mail.ru>
2023-03-18 12:14:03 +0300

    add 3 task

---
+++ task/longestPrefix.js
@@ -0,0 +1,18 @@
+/*
+
+*/
+
+var longestCommonPrefix = function (strs) {
+  strs.sort();
+
+  for (let i = 0; i < strs[0].length; i++) {
+    if (strs[0][i] !== strs[strs.length - 1][i]) {
+      return strs[0].substr(0, i);
+    }
+  }
+
+  return strs[0];
+};
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+console.log(longestCommonPrefix(["c", "acc", "ccc"]));
